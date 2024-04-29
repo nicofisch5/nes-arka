f398d2000adad6a335c3e27c6909cf31c0ac00bd
nico5 <nicofisch5@gmail.com>
2024-04-29 17:25:08 +0200

    Stick and ball in the middle after out of bounds

--- update-ball-position.asm
+++ update-ball-position.asm
@@ -157,17 +157,41 @@ CheckBallCollisionBottom:
   BEQ BallOutOfBounds
   JMP EndCheckBallCollisionBottom
 
-  BallOutOfBounds: ; The player should loose a life, for now we just move the ball
-    LDA #$10
-    STA SPR_BALL_ADDR
-    LDA #$20
-    STA SPR_BALL_ADDR+3
+  BallOutOfBounds:
     DEC nbLife
 
-    ; ************** END OF LIFE ****************
-    LDA nbLife
-    CMP #$0
-    BEQ PrepareGameOver
+    CheckEndOfLife:
+      LDA nbLife
+      CMP #$0
+      BEQ PrepareGameOver
+    EndCheckEndOfLife:
+
+    ; <START> Partie à remplacer par le fait que la balle soit collée au stick comme au départ
+    LDA #$CC
+    STA SPR_BALL_ADDR
+    LDA #$82
+    STA SPR_BALL_ADDR+3
+
+    ; Ball is stopped
+    LDA #$00
+    STA ballSpeed
+    STA balldown
+    STA ballleft
+    STA ballright
+
+    ; Stick in the middle
+    LDA #$70
+    STA SPR_STICK_ADDR+3
+    LDA #$79
+    STA SPR_STICK_ADDR+3+4
+    LDA #$82
+    STA SPR_STICK_ADDR+3+8
+    LDA #$8B
+    STA SPR_STICK_ADDR+3+12
+    LDA #$94
+    STA SPR_STICK_ADDR+3+16
+    ; <END> Partie à remplacer par le fait que la balle soit collée au stick comme au départ
+
     JMP EndCheckBallCollisionBottom
 
   PrepareGameOver:
